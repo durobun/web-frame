5b14a3c7576c69b8bacc699d9658a1dd365fdf4f
durobun <tojskim72@gmail.com>
2019-01-01 14:50:04 +0900

    modified blog

--- aweb/settings.py
+++ aweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9)87bke@cc!zkjh4k6#5t!@=l(tgc&$c98-ui&_eno519rtpl2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','juskim.iptime.org',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
 
 # Application definition
